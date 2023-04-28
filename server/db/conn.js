ea4d634ea749d4103733b2b80978f7e5ff7b1aa7
Anabel <anabel.batista@lehman.cuny.edu>
2023-04-27 22:24:11 -0400

    clean up

--- server/db/conn.js
+++ server/db/conn.js
@@ -18,7 +18,7 @@ module.exports = {
 //     client.connect(function (err, db) {
 //       // Verify we got a good "db" object
 //       if (db)
-//       {
+//       { Adding
 //         _db = db.db("employees");
 //         console.log("Successfully connected to MongoDB."); 
 //       }
